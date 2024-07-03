6ce1695291f072106c0fb92fbb1e59e89cef783a
Ashutosh Vimal <ashutoshvimal22@gmail.com>
2024-07-04 00:18:32 +0530

    1509. Minimum Difference Between Largest and Smallest Value in Three Moves

---
+++ 1509 Minimum Difference Between Largest and Smallest Value in Three Moves.cpp
@@ -0,0 +1,15 @@
+//1509. Minimum Difference Between Largest and Smallest Value in Three Moves
+
+class Solution {
+public:
+    int minDifference(vector<int>& nums) {
+        int n = nums.size();
+        if(n <= 4) return 0;
+
+        sort(nums.begin(), nums.end());
+
+        int res =  min({nums[n-4]-nums[0], nums[n-3]-nums[1],nums[n-2]-nums[2],nums[n-1]-nums[3]});
+        return res;
+
+    }
+};
